c5d29a8d89851eb0c128b1eda9ebe381777d5692
Srivaths Maharaj <Srivathsmaharaj@gmail.com>
2025-02-24 22:48:28 +0200

    Refactor layout and styles; implement task input functionality and remove unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,36 @@
 "use client";
+import { useState } from "react";
 
-import Link from "next/link";
+export default function Page() {
+  const [task, setTask] = useState("");
 
-export default function Home() {
-  // TODO: Learn how to use react useState
-
+  function handleChange(e) {
+    
+    setTask(e.target.value);
+  }
   return (
-    <div>
-      <Link
-        href="/about"
-        className="text-cyan-300 hover:text-blue-800 hover:underline"
-      >
-        about us
-      </Link>
+    <div className="w-screen flex flex-col items-center justify-center h-screen mx-auto bg-gradient-to-br from-purple-500 to-blue-400">
+      <div className="bg-white max-w-96 p-4 rounded-lg">
+        <h2>To do List</h2>
+        <div className="mx-auto">
+          <input
+            type="text"
+            id="input-box"
+            placeholder="add your task"
+            value={task}
+            onChange={handleChange}
+          />
+          <button
+            type="button"
+            className="bg-orange-500 px-2 rounded-lg text-sm py-1 text-white"
+            onClick={(e) => {
+              console.log(task);
+            }}
+          >
+            Add
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
